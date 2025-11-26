5417e174a669bcd4e5a40d7dcca454b17acfb4c9
Joe Bowbeer <joe.bowbeer@gmail.com>
2025-11-26 15:55:36 -0800

    Create cli.spec.js

---
+++ test/cli.spec.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai")
+const { execSync } = require("node:child_process")
+
+const test = (input, args) => {
+  return execSync(`node cli.js ${args}`, { input }).toString()
+}
+
+describe("CLI", () => {
+  it("should run", () => {
+    expect(test("{}", "-p foo")).to.equal("{}\n")
+  })
+})
